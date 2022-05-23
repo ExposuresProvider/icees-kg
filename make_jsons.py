e6289af5322a75ec6ecc07e35abb92a9e07f02a0
Max Wang <max@covar.com>
2022-05-23 14:52:19 -0400

    Fix a few bugs in frequencies and percentages functions

--- make_jsons.py
+++ make_jsons.py
@@ -53,7 +53,7 @@ for feature, feature_info in features.items():
     # Figure out if this feature is in the data_columns
     try:
         feature_column_index = data_columns.index(feature)
-    except:
+    except ValueError:
         # This yaml feature is not found in the data csv
         continue
 
@@ -69,7 +69,7 @@ for feature, feature_info in features.items():
             'enum': feature_info['enum'],
             'range': len(feature_info['enum']),
             'min': 0,
-            'max': len(feature_info['enum'])-1,
+            'max': len(feature_info['enum']) - 1,
             'is_integer': False,
             'categories': feature_info.get('categories', [])
         }
@@ -117,45 +117,50 @@ is_useful_feature = np.sum(np.isfinite(data_np), axis=0) > 0
 useful_features = [k for i, k in enumerate(data_column_info.keys()) if is_useful_feature[i]]
 
 
-def get_feature_count_matrix(x1, x2, u_x1, u_x2):
+def get_feature_count_matrix(x1, x2, u_x1, u_x2, feature_1, feature_2):
     # TODO: This won't handle cases where there are invalid values in the data
-    
-    feat_count_mat = np.zeros((len(u_x1) - 1, len(u_x2) - 1))
+    feat_count_mat = np.zeros((len(u_x2), len(u_x1)))
 
+    errors = False
     for cx1, cx2 in zip(x1, x2):
         if (not np.isnan(cx1)) and (not np.isnan(cx2)):
-            i_x1 = np.where(u_x1 == cx1)[0][0]
-            i_x2 = np.where(u_x2 == cx2)[0][0]
+            try:
+                i_x1 = np.where(u_x1 == cx1)[0][0]
+                i_x2 = np.where(u_x2 == cx2)[0][0]
 
-            feat_count_mat[i_x2, i_x1] += 1 # x2 should be on the rows and x1 on the cols
+                feat_count_mat[i_x2, i_x1] += 1  # x2 should be on the rows and x1 on the cols
+            except IndexError as e:
+                errors = True
+                pass
+    if errors:
+        LOGGER.warning(f"Computed count matrix with errors for {feature_1} and {feature_2}")
 
     row_count = np.sum(feat_count_mat, axis=0)
     row_density = row_count / np.sum(row_count)
 
     # This is a little confusing because when you sum through the rows, you have something the length of the columns
-    col_summary = [{"frequency": row_count[ind], "percentage": row_count[ind]} for ind in range(row_count.size)], # cols will refer to feature_a (x1)
-
+    col_summary = [{"frequency": row_count[ind], "percentage": row_count[ind]} for ind in range(row_count.size)],  # cols will refer to feature_a (x1)
 
     col_count = np.sum(feat_count_mat, axis=1)
     col_density = col_count / np.sum(col_count)
-    
+
     # This is a little confusing because when you sum through the columns, you have something the length of the rows
-    row_summary = [{"frequency": col_count[ind], "percentage": col_density[ind]} for ind in range(col_count.size)], # rows will refer to feature_b (x2)
-    
+    row_summary = [{"frequency": col_count[ind], "percentage": col_density[ind]} for ind in range(col_count.size)],  # rows will refer to feature_b (x2)
 
     return feat_count_mat, row_summary, col_summary
 
+
 def get_feature_info_from_column_info(column_info):
-    
+    """Get feature qualifiers from column info."""
     if column_info['is_integer']:
         feature_qualifiers = [
             {"operator": "=", "value": i + column_info['min']}
-            for i in column_info['range']
+            for i in range(column_info['range'])
         ]
-    else: # enum
+    else:  # enum
         feature_qualifiers = [
             {"operator": "=", "value": k}
-            for k in column_info['enum'].keys()
+            for k in column_info['enum']
         ]
 
     return {
@@ -163,17 +168,18 @@ def get_feature_info_from_column_info(column_info):
         'feature_qualifiers': feature_qualifiers
     }
 
+
 def get_feature_matrix_json(count_mat):
-    
+    """Get feature matrix from count matrix."""
     row_counts = np.sum(count_mat, axis=1)
     total = np.sum(row_counts)
     col_counts = np.sum(count_mat, axis=0)
-    
+
     matrix_dict = []
-    for i in count_mat.shape[0]:
+    for i in range(count_mat.shape[0]):
         row = []
-        for j in count_mat.shape[1]:
-            v = count_mat[i,j]
+        for j in range(count_mat.shape[1]):
+            v = count_mat[i, j]
             row.append({
                 "frequency": v,
                 "row_percentage": v / row_counts[i],
@@ -181,10 +187,8 @@ def get_feature_matrix_json(count_mat):
                 "total_percentage": v / total,
             })
         matrix_dict.append(row)
-    
 
     return matrix_dict
-
 
 
 node_list = []
@@ -207,8 +211,8 @@ for i_col, (i_column, i_column_info) in enumerate(tqdm(data_column_info.items())
     x1 = data_np[:, i_col]
     if i_column_info['is_integer']:
         u_x1 = [ind + j_column_info['min'] for ind in range(i_column_info['range'])]
-    else: # enum
-        u_x1 = range(len(i_column_info['enum']))
+    else:  # enum
+        u_x1 = list(range(len(i_column_info['enum'])))
 
     # DEBUG: save names of normalized nodes
     if NORMALIZE:
@@ -270,13 +274,13 @@ for i_col, (i_column, i_column_info) in enumerate(tqdm(data_column_info.items())
         x2 = data_np[:, j_col]
         if j_column_info['is_integer']:
             u_x2 = [ind + j_column_info['min'] for ind in range(j_column_info['range'])]
-        else: # enum
-            u_x2 = range(len(j_column_info['enum']))
-       
+        else:  # enum
+            u_x2 = list(range(len(j_column_info['enum'])))
+
         # Calculate stats for i_col and j_col
         # x1 is the column_sum
         # x2 is the row_sum
-        count_mat, row_summary, col_summary = get_feature_count_matrix(x1, x2, u_x1, u_x2)
+        count_mat, row_summary, col_summary = get_feature_count_matrix(x1, x2, u_x1, u_x2, i_column, j_column)
         chi_squared, p, *_ = chi2_contingency(count_mat + np.finfo(np.float32).eps, correction=False)
 
         # Package edge properties
@@ -284,7 +288,7 @@ for i_col, (i_column, i_column_info) in enumerate(tqdm(data_column_info.items())
             'feature_a': feature_description_1,
             'feature_b': feature_description_2,
             'feature_matrix': get_feature_matrix_json(count_mat),
-            'rows': row_summary, 
+            'rows': row_summary,
             'columns': col_summary,
             "total": np.sum(count_mat),
             "chi_squared": chi_squared,
